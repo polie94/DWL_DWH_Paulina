0ac991ba9845937ac443a2ba61fe2e516733456b
polie94 <40672835+polie94@users.noreply.github.com>
2023-06-24 13:10:30 +0200

    Create configs.py

---
+++ Data Lake/Air Pollution/configs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 09:56:40 2023
+
+@author: paulina
+"""
+
+API_KEY=###placeholder
+S3_BUCKET_NAME = "lakeprojecttexasv2"
+OBJECT_KEY = "Texas_Counties_Centroid_Map.csv"
+ACCESS_KEY_ID = ###placeholder
+SECRET_ACCESS_KEY=###placeholder
+SESSION_TOKEN= ###placeholder
